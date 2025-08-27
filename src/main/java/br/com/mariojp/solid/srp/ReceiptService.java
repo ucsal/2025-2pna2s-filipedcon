ba46f61867886e127c62ecc38e6467bb10d9541a
lipe <liipelovemoon@gmail.com>
2025-08-27 16:08:28 -0300

    feat: inicializar tax para receber propriedade tax.rate

--- src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
+++ src/main/java/br/com/mariojp/solid/srp/ReceiptService.java
@@ -3,11 +3,13 @@ package br.com.mariojp.solid.srp;
 public class ReceiptService {
 	public String generate(Order order) {
 		TaxCalculator taxCalcObjeto = new TaxCalculator();
+		String taxRateString = System.getProperty("tax.rate");
+		double tax = Double.parseDouble(taxRateString);
 		double subtotal = order.getItems().stream().mapToDouble(i -> i.getUnitPrice() * i.getQuantity()).sum();
-		double tax = taxCalcObjeto.calcTax(subtotal, 0.38);
-		double total = subtotal + tax;
+		double taxEmValor = taxCalcObjeto.calcTax(subtotal, tax);
+		double total = subtotal + taxEmValor;
 		ReceiptFormatter montaRecibo = new ReceiptFormatter();
-		StringBuilder recibo = montaRecibo.montaRecibo(order, subtotal, tax, total);
+		StringBuilder recibo = montaRecibo.montaRecibo(order, subtotal, taxEmValor, total);
 		return recibo.toString();
 	}
 }
